b80eccb7f0991a15b29e2975b25e03a3b571adf3
Abderahmane Marega <abder.marega@gmail.com>
2024-01-11 16:10:57 +0100

    jour 3 exo 2

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $produits = $productRepository->findAll();
+
         return $this->render('base.html.twig', [
             'controller_name' => 'HomepageController',
+            'produits' => $produits,
         ]);
     }
 }
--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use LDAP\Result;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,11 +16,19 @@ class ProduitsController extends AbstractController
     #[Route('/produits', name: 'app_produits')]
 
 
-    public function new(Request $request): Response {
+    public function new(Request $request, ManagerRegistry $doctrine): Response {
 
         $produit = new Product();
         $form = $this->createForm(AjoutProduitType::class, $produit);
         $form->handleRequest($request); 
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($produit);
+            $entityManager->flush();
+        } 
+
+
 
         return $this->render(
             'produits/index.html.twig',
