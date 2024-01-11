86551cec15be375d2daeffd8b440f78f6f7eca02
Abderahmane Marega <abder.marega@gmail.com>
2024-01-11 11:51:22 +0100

    jour 3

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use LDAP\Result;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use App\Form\AjoutProduitType;
 
 class ProduitsController extends AbstractController
 {
     #[Route('/produits', name: 'app_produits')]
-    public function index(): Response
-    {
-        return $this->render('produits/index.html.twig', [
-            'controller_name' => 'ProduitsController',
-        ]);
+
+
+    public function new(Request $request): Response {
+
+        $produit = new Product();
+        $form = $this->createForm(AjoutProduitType::class, $produit);
+        $form->handleRequest($request); 
+
+        return $this->render(
+            'produits/index.html.twig',
+            ['formProduit' => $form->createView()]
+        );
     }
 }
